b6ea234b84dc6a7cc743b9a6179f10b8592f60ff
kuahn <z@kuahn.com>
2012-09-02 16:18:34 +0900

    first commit

---
+++ src/com/kitty/core/core.java
@@ -0,0 +1,11 @@
+package com.kitty.core;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: kuahn
+ * Date: 12. 9. 2.
+ * Time: 오후 4:18
+ * To change this template use File | Settings | File Templates.
+ */
+public class core {
+}
